429e9becb556644786c74b399cc251d28d87dc22
Robin Lindén <mail+gitlab@robinlinden.eu>
2017-01-09 18:12:23 +0100

    Refactor out pr-table js into its own file. Add easy way of properly including js. Add folder for js content.

---
+++ toktok/static/js/pr-table.js
@@ -0,0 +1,75 @@
+// Function depends on twemoji already having been loaded.
+function createPrTable() {
+  var repoSection = document.querySelector('main');
+  var requestHeaders = new Headers();
+  var requestInit = { method: 'GET',
+                      headers: requestHeaders,
+                      mode: 'cors',
+                      cache: 'default' };
+
+  var stateIcon = {
+    clean: '&#x2705;',
+    dirty: '&#x274c;',
+    unknown: '&#x231b;',
+    unstable: '&#x1f6a7;',
+  };
+
+  fetch('https://git-critique.herokuapp.com/hello/pulls', requestInit)
+  .then(function(response) { return response.json(); })
+  .then(function(json) {
+    var loadingDiv = repoSection.querySelector("div.loading");
+    loadingDiv.parentNode.removeChild(loadingDiv);
+
+    for (var i = 0; i < json.length; i++) {
+      if (json[i].length > 0) {
+        var repoTitle = document.createElement('h2');
+        repoTitle.innerHTML = json[i][0].prRepoName;
+        repoSection.appendChild(repoTitle);
+
+        var prTable = document.createElement('table');
+        prTable.className = "pr-table"
+        var prHeaders = document.createElement('tr');
+        prHeaders.innerHTML =
+          "<th>" + "#"         + "</th>" +
+          "<th>" + "Branch"    + "</th>" +
+          "<th>" + "Title"     + "</th>" +
+          "<th>" + "State"     + "</th>" +
+          "<th>" + "Reviewers" + "</th>";
+        prTable.appendChild(prHeaders);
+
+        for (var j = 0; j < json[i].length; j++) {
+          var listItem = document.createElement('tr');
+          var prLink =
+            "<a href='https://github.com/TokTok/" + json[i][j].prRepoName +
+            "/pull/" + json[i][j].prNumber + "'>" + json[i][j].prNumber +
+            "</a>";
+          var reviewableLink =
+            " <a href='https://reviewable.io/reviews/toktok/" + json[i][j].prRepoName +
+            "/" + json[i][j].prNumber + "'>&#x1f50d;</a>";
+          var branchWithTooltip =
+            "<div class='tooltip'>" + json[i][j].prBranch +
+            "<span class='tooltiptext'>" + json[i][j].prUser + "</span>" +
+            "</div>";
+          listItem.innerHTML =
+            "<td>" + prLink + reviewableLink           + "</td>" +
+            "<td>" + branchWithTooltip                 + "</td>" +
+            "<td>" + json[i][j].prTitle                + "</td>" +
+            "<td>" + stateIcon[json[i][j].prState]     + "</td>" +
+            "<td>" + json[i][j].prReviewers.join(", ") + "</td>";
+          prTable.appendChild(listItem);
+        }
+        repoSection.appendChild(prTable);
+
+        // parse emoji in the content to ensure it gets displayed correctly in all browsers
+        twemoji.size = '16x16'; // This can be set to 16x16, 36x36, or 72x72
+        twemoji.parse(prTable, {
+            callback: function(icon, options) {
+              return relative + 'static/img/emoij/' + options.size + '/' + icon + '.png';
+            }
+        });
+      }
+    }
+  });
+}
+
+createPrTable();
